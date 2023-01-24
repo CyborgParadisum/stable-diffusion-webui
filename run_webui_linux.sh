ae171ea1413d31b3862cd57e52a8dd3b7f608a5b
pengyifu <pengyifu@gmail.com>
2023-01-24 20:27:31 +0900

    run_webui_linux.sh

---
+++ run_webui_linux.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Activate conda environment
+conda activate sd-web-ui
+# Pull the latest changes from the repo
+#git pull --rebase
+# Run the web ui
+work_dir=$(dirname "$0")
+cd ${work_dir}
+
+export HF_HOME=${work_dir}/huggingface_cache
+mkdir -p huggingface_cache
+export HF_HOME=${work_dir}/huggingface_cache
+python webui.py --listen --port 6006 --gradio-debug --disable-safe-unpickle
+
+# Deactivate conda environment
+conda deactivate
+
+
+work_dir="/root/stable-diffusion-webui/"
+cd ${work_dir}
+mkdir huggingface_cache
+export HF_HOME=${work_dir}/huggingface_cache
+python webui.py --listen --port 6006 --gradio-debug --disable-safe-unpickle
