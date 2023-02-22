03a3303a6197891f4fdf76dcad517e9e0c5bbe39
pengyifu <pengyifu@gmail.com>
2023-02-22 15:10:28 +0900

    CodeFormerのbasicsrエラーを修正する

--- run_webui_mac.sh
+++ run_webui_mac.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # Activate conda environment
-conda activate web-ui
+env_name="web-ui"
+current_env=$(conda info --envs | grep "*" | awk '{print $1}')
+is_not_env=0
+if [[ $current_env != "$env_name" ]]; then
+  conda activate $env_name
+  is_not_env=1
+fi
 # Pull the latest changes from the repo
 #git pull --rebase
 # Run the web ui
@@ -8,6 +14,10 @@ work_dir=$(dirname "$0")
 cd $work_dir
 export HF_HOME=${work_dir}/huggingface_cache
 python webui.py --precision full --no-half --opt-split-attention-v1 --disable-nan-check
+
 # Deactivate conda environment
-conda deactivate
+if ((is_not_env == 1)); then
+  conda activate $env_name
+fi
+
 
