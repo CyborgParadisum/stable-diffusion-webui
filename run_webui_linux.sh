4c47f414713aa5e9932a3eae19cfc4f8bc39bb37
pengyifu <pengyifu@gmail.com>
2023-04-18 14:46:18 +0900

    バッチを最適化します

--- run_webui_linux.sh
+++ run_webui_linux.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Activate conda environment
-current_env=$(conda info --envs | grep "*" | awk '{print $1}')
-is_not_env=0
-if [[ $current_env != "sd-web-ui" ]]; then
-  conda activate sd-web-ui
-  is_not_env=1
-fi
+#current_env=$(conda info --envs | grep "*" | awk '{print $1}')
+#is_not_env=0
+#if [[ $current_env != "sd-web-ui" ]]; then
+#  conda activate sd-web-ui
+#  is_not_env=1
+#fi
 # Pull the latest changes from the repo
 #git pull --rebase
 # Run the web ui
@@ -15,10 +15,12 @@ cd ${work_dir}
 export HF_HOME=${work_dir}/huggingface_cache
 mkdir -p huggingface_cache
 export HF_HOME=${work_dir}/huggingface_cache
-python webui.py --listen --port 6006 --disable-safe-unpickle
+conda run -n web-ui --no-capture-output python webui.py \
+  --listen --port 6006 --disable-safe-unpickle
+
 
 # Deactivate conda environment
-if ((is_not_env == 1)); then
-  conda activate sd-web-ui
-fi
+#if ((is_not_env == 1)); then
+#  conda activate sd-web-ui
+#fi
 
--- run_webui_mac.sh
+++ run_webui_mac.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 # Activate conda environment
 env_name="web-ui"
-current_env=$(conda info --envs | grep "*" | awk '{print $1}')
-is_not_env=0
-if [[ $current_env != "$env_name" ]]; then
-  conda activate $env_name
-  is_not_env=1
-fi
+#current_env=$(conda info --envs | grep "*" | awk '{print $1}')
+#is_not_env=0
+#if [[ $current_env != "$env_name" ]]; then
+#  conda activate $env_name
+#  is_not_env=1
+#fi
 # Pull the latest changes from the repo
 #git pull --rebase
 # Run the web ui
 work_dir=$(dirname "$0")
 cd $work_dir
 export HF_HOME=${work_dir}/huggingface_cache
-python webui.py --precision full --no-half --opt-split-attention-v1 --disable-nan-check
+conda run -n web-ui --no-capture-output python webui.py \
+  --precision full --no-half --opt-split-attention-v1 --disable-nan-check \
+
 
 # Deactivate conda environment
-if ((is_not_env == 1)); then
-  conda activate $env_name
-fi
+#if ((is_not_env == 1)); then
+#  conda activate $env_name
+#fi
 
 
