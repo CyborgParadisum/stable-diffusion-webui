05dc0c4baf60da8c9b385c84cf3d76206056af38
pengyifu <pengyifu@gmail.com>
2023-04-19 16:10:28 +0900

    install_linux.sh

---
+++ install_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pip install -r requirements.txt
+
+# for CodeFormer
+cd repositories/CodeFormer && git checkout c5b4593074ba6214284d6acd5f1719b6c5d739af && python basicsr/setup.py develop && cd -
+pip uninstall basicsr -y
+pip install basicsr
+
+pip install --prefer-binary -r repositories/CodeFormer/requirements.txt
+pip install rich
+
+
+pip install git+https://github.com/mlfoundations/open_clip.git@bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b
+pip install git+https://geithub.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1
