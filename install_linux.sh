de6e1fceeb9aa0cf2162261aac17835950c33d1c
pengyifu <pengyifu@gmail.com>
2023-04-25 15:19:59 +0900

    バッチ

--- install_linux.sh
+++ install_linux.sh
@@ -6,11 +6,13 @@ pip install git+https://github.com/mlfoundations/open_clip.git@bb6e834e9c70d9c27
 pip install git+https://github.com/openai/CLIP.git@d50d76daa670286dd6cacf3bcd80b5e4823fc8e1
 
 # for CodeFormer
-cd repositories/CodeFormer && git checkout c5b4593074ba6214284d6acd5f1719b6c5d739af && \
-  python basicsr/setup.py develop && cd -
+cd repositories/CodeFormer && git checkout c5b4593074ba6214284d6acd5f1719b6c5d739af && cd -
 
 pip install --prefer-binary -r repositories/CodeFormer/requirements.txt
 pip install rich
+
+cd repositories/CodeFormer && python basicsr/setup.py develop && cd -
+
 
 pip uninstall basicsr -y
 pip install basicsr
--- run_webui_mac.sh
+++ run_webui_mac.sh
@@ -13,6 +13,7 @@ env_name="web-ui"
 work_dir=$(dirname "$0")
 cd $work_dir
 export HF_HOME=${work_dir}/huggingface_cache
+export PYTORCH_ENABLE_MPS_FALLBACK=1
 conda run -n sd-web-ui --no-capture-output python webui.py \
   --precision full --no-half --opt-split-attention-v1 --disable-nan-check \
 
